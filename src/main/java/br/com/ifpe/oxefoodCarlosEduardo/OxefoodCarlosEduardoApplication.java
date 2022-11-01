78bf87a0a634012d089d1244bed4cf65e61ffaf9
Carlos_Eduardo <carlos_eduardo565@hotmail.com>
2022-11-01 16:54:52 -0300

    commit do projeto de WEB IV

---
+++ src/main/java/br/com/ifpe/oxefoodCarlosEduardo/OxefoodCarlosEduardoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodCarlosEduardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodCarlosEduardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodCarlosEduardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodCarlosEduardo/OxefoodCarlosEduardoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodCarlosEduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodCarlosEduardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
